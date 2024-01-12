8a3ca670b385aba07e918907211e9f127af28160
azaz0 <nanquery@gmail.com>
2024-01-12 22:42:42 +0100

    allow searching by date (from and/or to)

--- expenses/forms.py
+++ expenses/forms.py
@@ -3,10 +3,19 @@ from .models import Expense
 
 
 class ExpenseSearchForm(forms.ModelForm):
+    date_from = forms.DateField(
+        widget=forms.DateInput(
+            attrs={'type': 'date', 'data-clear-btn': 'true'}))
+    date_to = forms.DateField(
+        widget=forms.DateInput(
+            attrs={'type': 'date', 'data-clear-btn': 'true'}))
+
     class Meta:
         model = Expense
-        fields = ('name',)
+        fields = ('name', 'date_from', 'date_to')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['name'].required = False
+        self.fields['date_from'].required = False
+        self.fields['date_to'].required = False
--- expenses/views.py
+++ expenses/views.py
@@ -1,22 +1,51 @@
-from django.views.generic.list import ListView
-
 from .forms import ExpenseSearchForm
 from .models import Expense, Category
 from .reports import summary_per_category
+from django.core.paginator import Paginator
+from django.views.generic.list import ListView
 
 
 class ExpenseListView(ListView):
+    """
+    View displaying a list of expenses.
+
+    Attributes:
+        model (Expense): The model for the view.
+        paginate_by (int): Number of items per page.
+
+    Methods:
+        get_context_data: Retrieve the context data for rendering the view.
+    """
+
     model = Expense
     paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
+        """
+        Get the context data for rendering the expense list view.
+
+        Args:
+            object_list (QuerySet): List of objects.
+
+        Returns:
+            dict: Context data for rendering the view.
+        """
         queryset = object_list if object_list is not None else self.object_list
 
         form = ExpenseSearchForm(self.request.GET)
         if form.is_valid():
             name = form.cleaned_data.get('name', '').strip()
+            date_from = form.cleaned_data.get('date_from')
+            date_to = form.cleaned_data.get('date_to')
+
             if name:
                 queryset = queryset.filter(name__icontains=name)
+            if date_from and date_to:
+                queryset = queryset.filter(date__range=[date_from, date_to])
+            if date_from:
+                queryset = queryset.filter(date__gte=date_from)
+            if date_to:
+                queryset = queryset.filter(date__lte=date_to)
 
         return super().get_context_data(
             form=form,
@@ -24,7 +53,15 @@ class ExpenseListView(ListView):
             summary_per_category=summary_per_category(queryset),
             **kwargs)
 
+
 class CategoryListView(ListView):
+    """
+    View displaying a list of categories.
+
+    Attributes:
+        model (Category): The model for the view.
+        paginate_by (int): Number of items per page.
+    """
+
     model = Category
     paginate_by = 5
-
