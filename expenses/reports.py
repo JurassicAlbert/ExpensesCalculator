55a9ae605c50aee3e23755328ea130fa3fcc4383
azaz0 <nanquery@gmail.com>
2024-01-13 18:39:23 +0100

    add docstrings and optimalize

--- expenses/reports.py
+++ expenses/reports.py
@@ -1,10 +1,19 @@
 from collections import OrderedDict
 from django.db.models import Sum, Value, Count
-from django.db.models.functions import Coalesce
-from django.db.models.functions import ExtractYear, ExtractMonth
+from django.db.models.functions import Coalesce, ExtractYear, ExtractMonth
 
 
 def summary_per_category(queryset):
+    """
+    Calculate the summary of expenses per category.
+
+    Args:
+        queryset (QuerySet): List of Expense objects.
+
+    Returns:
+        OrderedDict: A dictionary containing category names and their total
+        amounts.
+    """
     return OrderedDict(sorted(
         queryset
         .annotate(category_name=Coalesce('category__name', Value('-')))
@@ -16,6 +25,16 @@ def summary_per_category(queryset):
 
 
 def summary_per_year_month(queryset):
+    """
+    Calculate the summary of expenses per year-month.
+
+    Args:
+        queryset (QuerySet): List of Expense objects.
+
+    Returns:
+        list: A list of tuples containing year, month, and total amount for
+        each year-month.
+    """
     return queryset.values('date__year', 'date__month').annotate(
         year=ExtractYear('date'),
         month=ExtractMonth('date'),
@@ -24,4 +43,26 @@ def summary_per_year_month(queryset):
 
 
 def expenses_per_category(queryset):
+    """
+    Annotate the number of expenses per category.
+
+    Args:
+        queryset (QuerySet): List of Category objects.
+
+    Returns:
+        QuerySet: Annotated QuerySet with the number of expenses per category.
+    """
     return queryset.annotate(num_expenses=Count('expense'))
+
+
+def expenses_total_amount(queryset):
+    """
+    Calculate the total amount of expenses.
+
+    Args:
+        queryset (QuerySet): List of Expense objects.
+
+    Returns:
+        Decimal: The total amount of expenses.
+    """
+    return queryset.aggregate(total_amount=Sum('amount'))['total_amount']
--- expenses/views.py
+++ expenses/views.py
@@ -1,9 +1,9 @@
-from django.db.models import Sum
+from django.db.models import Q
 from .forms import ExpenseSearchForm
 from .models import Expense, Category
 from django.views.generic.list import ListView
-from .reports import summary_per_category, summary_per_year_month, \
-    expenses_per_category
+from .reports import (summary_per_category, summary_per_year_month,
+                      expenses_per_category, expenses_total_amount)
 
 
 class ExpenseListView(ListView):
@@ -35,44 +35,62 @@ class ExpenseListView(ListView):
 
         form = ExpenseSearchForm(self.request.GET)
         if form.is_valid():
+
             name = form.cleaned_data.get('name', '').strip()
             date_from = form.cleaned_data.get('date_from')
             date_to = form.cleaned_data.get('date_to')
             categories = form.cleaned_data.get('categories')
 
+            filters = Q()
             if name:
-                queryset = queryset.filter(name__icontains=name)
+                filters &= Q(name__icontains=name)
             if date_from and date_to:
-                queryset = queryset.filter(date__range=[date_from, date_to])
+                filters &= Q(date__range=[date_from, date_to])
             if date_from:
-                queryset = queryset.filter(date__gte=date_from)
+                filters &= Q(date__gte=date_from)
             if date_to:
-                queryset = queryset.filter(date__lte=date_to)
+                filters &= Q(date__lte=date_to)
             if categories:
-                queryset = queryset.filter(category__in=categories)
+                filters &= Q(category__in=categories)
+
+            queryset = queryset.filter(filters)
 
         sort = self.request.GET.get('sort')
         if sort:
             queryset = queryset.order_by(sort)
 
-        total_amount = queryset.aggregate(total_amount=Sum('amount'))[
-            'total_amount']
-
         return super().get_context_data(
             form=form,
             object_list=queryset,
-            total_amount=total_amount,
+            total_amount=expenses_total_amount(queryset),
             summary_per_category=summary_per_category(queryset),
             summary_per_year_month=summary_per_year_month(queryset),
             **kwargs)
 
 
 class CategoryListView(ListView):
+    """
+    View displaying a list of categories.
+
+    Attributes:
+        model (Category): The model for the view.
+        paginate_by (int): Number of items per page.
+        template_name (str): Name of the template for rendering the view.
+    """
+
     model = Category
     paginate_by = 5
-    template_name = 'category_list.html'  # Podaj nazwę swojego szablonu
 
     def get_context_data(self, *, object_list=None, **kwargs):
+        """
+        Get the context data for rendering the category list view.
+
+        Args:
+            object_list (QuerySet): List of objects.
+
+        Returns:
+            dict: Context data for rendering the view.
+        """
         queryset = object_list if object_list is not None else self.object_list
 
         queryset = expenses_per_category(queryset)
